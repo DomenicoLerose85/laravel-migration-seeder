299252e040782505a47df92420c4d1de4b583547
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-14 17:01:48 +0200

    migration_created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('layouts.default')
+
+@section('pageTitle', 'Home')
+
+@section('mainContent')
     <h1 class="text-center">Ciao <i class="fa-solid fa-code-commit"></i></h1>
-</body>
-</html>
+    
+@endsection
